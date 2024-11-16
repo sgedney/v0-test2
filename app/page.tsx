065f938b119bc0cdc7b3ddd191be3a6176d3df11
Suzanne Gedney <sgedney@users.noreply.github.com>
2024-11-16 09:55:58 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,3 +3,4 @@ import { ElegantLandingPageComponent } from "@/components/elegant-landing-page"
 export default function Page() {
   return <ElegantLandingPageComponent />
 }
+// Triggering new Vercel deployment
